deafc312a963732086eed687a4b1747759ac6a11
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-02-25 10:57:44 +0000

    finalisation du model colocation

--- FacileColoc/app/Models/Colocation.php
+++ FacileColoc/app/Models/Colocation.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Colocation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status',
+        'cancelled_at',
+        'owner_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'colocation_user')
+            ->withPivot(['role', 'joined_at', 'left_at', 'is_active'])
+            ->withTimestamps();
+    }
 }
