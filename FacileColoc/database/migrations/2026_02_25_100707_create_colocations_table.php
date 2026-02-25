9d7666f552ef374310327920fc744c4a2b72b49a
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-02-25 10:55:42 +0000

    finalisation de la migration create colocation table

--- FacileColoc/database/migrations/2026_02_25_100707_create_colocations_table.php
+++ FacileColoc/database/migrations/2026_02_25_100707_create_colocations_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('status')->default('active'); // active | cancelled
+            $table->timestamp('cancelled_at')->nullable();
+
+            $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('colocations');
