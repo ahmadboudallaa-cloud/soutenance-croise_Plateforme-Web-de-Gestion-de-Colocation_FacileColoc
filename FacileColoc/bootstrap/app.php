c38779b3549017bb656a732d89eaa591434a1b58
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-02-25 10:52:22 +0000

    finalisation du fichier app.php dans le dossier bootstrap

--- FacileColoc/bootstrap/app.php
+++ FacileColoc/bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -10,9 +9,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'banned' => \App\Http\Middleware\EnsureUserNotBanned::class,
+        ]);
+
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+
+    ->create();
--- FacileColoc/routes/web.php
+++ FacileColoc/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth', 'banned'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
