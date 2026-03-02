4f5f38cd41fbb48391e141b7f732dc9a61b3750a
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-02 09:54:17 +0000

    Add FacilColoc/app/Http/Middleware/

---
+++ FacilColoc/app/Http/Middleware/EnsureNotBanned.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureNotBanned
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->isBanned()) {
+            auth()->logout();
+
+            return redirect('/')
+                ->with('error', 'Votre compte a été désactivé.');
+        }
+
+        return $next($request);
+    }
+}
