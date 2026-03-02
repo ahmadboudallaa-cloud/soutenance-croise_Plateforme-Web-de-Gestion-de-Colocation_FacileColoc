fcd58920641b609baaef60de592bd5e984616722
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-02 15:20:31 +0000

    Update FacilColoc/resources/views/layouts/guest.blade.php

--- FacilColoc/resources/views/layouts/guest.blade.php
+++ FacilColoc/resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EasyColoc</title>
+    <title>FacileColoc</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet">
@@ -64,7 +64,7 @@
 <body>
 
 <div class="container py-5">
-    <div class="text-center mb-4 brand">EasyColoc</div>
+    <div class="text-center mb-4 brand">FacileColoc</div>
     @yield('content')
 </div>
 
