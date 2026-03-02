2e9d0883574b053ba5ad143ff76e7ef443e533e3
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-02 13:18:05 +0000

    chore: update routes

--- FacilColoc/routes/web.php
+++ FacilColoc/routes/web.php
@@ -6,10 +6,21 @@ use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/invitations/{token}',
+    [InvitationController::class, 'show']
+)->name('invitations.show');
+
+Route::get(
+    '/join/{token}',
+    [InvitationController::class, 'show']
+)->name('colocations.join');
 
 require __DIR__.'/auth.php';
 
@@ -55,15 +66,10 @@ Route::middleware(['auth', 'not_banned'])->group(function () {
         [ColocationController::class, 'removeMember']
     )->name('colocations.members.remove');
 
-    Route::get(
-        '/join/{token}',
-        [InvitationController::class, 'show']
-    )->name('colocations.join');
-
-    Route::get(
-        '/invitations/{token}',
-        [InvitationController::class, 'show']
-    )->name('invitations.show');
+    Route::post(
+        '/colocations/{colocation}/members/{user}/transfer-owner',
+        [ColocationController::class, 'transferOwner']
+    )->name('colocations.members.transfer');
 
     Route::post(
         '/invitations/{token}/accept',
@@ -80,6 +86,11 @@ Route::middleware(['auth', 'not_banned'])->group(function () {
         [InvitationController::class, 'store']
     )->name('colocations.invite');
 
+    Route::delete(
+        '/colocations/{colocation}/invitations/{invitation}',
+        [InvitationController::class, 'destroy']
+    )->name('colocations.invitations.destroy');
+
     /*
     |--------------------------------------------------------------------------
     | EXPENSES
@@ -95,6 +106,21 @@ Route::middleware(['auth', 'not_banned'])->group(function () {
         [ExpenseController::class, 'store']
     )->name('expenses.store');
 
+    Route::get(
+        '/colocations/{colocation}/expenses/{expense}/edit',
+        [ExpenseController::class, 'edit']
+    )->name('expenses.edit');
+
+    Route::patch(
+        '/colocations/{colocation}/expenses/{expense}',
+        [ExpenseController::class, 'update']
+    )->name('expenses.update');
+
+    Route::delete(
+        '/colocations/{colocation}/expenses/{expense}',
+        [ExpenseController::class, 'destroy']
+    )->name('expenses.destroy');
+
     /*
     |--------------------------------------------------------------------------
     | PAYMENTS
@@ -104,6 +130,18 @@ Route::middleware(['auth', 'not_banned'])->group(function () {
         '/payments',
         [PaymentController::class, 'store']
     )->name('payments.store');
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROFILE
+    |--------------------------------------------------------------------------
+    */
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 
     /*
     |--------------------------------------------------------------------------
@@ -120,6 +158,21 @@ Route::middleware(['auth', 'not_banned'])->group(function () {
 
             Route::get('/users', [AdminController::class, 'users'])
                 ->name('users.index');
+
+            Route::get('/users/create', [AdminController::class, 'create'])
+                ->name('users.create');
+
+            Route::post('/users', [AdminController::class, 'store'])
+                ->name('users.store');
+
+            Route::get('/users/{user}/edit', [AdminController::class, 'edit'])
+                ->name('users.edit');
+
+            Route::patch('/users/{user}', [AdminController::class, 'update'])
+                ->name('users.update');
+
+            Route::delete('/users/{user}', [AdminController::class, 'destroy'])
+                ->name('users.destroy');
 
             Route::post('/users/{user}/ban', [AdminController::class, 'ban'])
                 ->name('users.ban');
