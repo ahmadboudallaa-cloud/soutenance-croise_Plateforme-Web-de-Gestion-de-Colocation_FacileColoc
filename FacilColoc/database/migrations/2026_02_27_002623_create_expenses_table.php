8724c7a29db35c682e01d3adaf49ee3f3e7989c2
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-02 09:54:22 +0000

    Add FacilColoc/database/migrations/2026_02_27_002623_create_expenses_table.php

---
+++ FacilColoc/database/migrations/2026_02_27_002623_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('colocation_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('paid_by')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->string('title');
+
+            $table->decimal('amount', 10, 2);
+
+            $table->date('expense_date');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
