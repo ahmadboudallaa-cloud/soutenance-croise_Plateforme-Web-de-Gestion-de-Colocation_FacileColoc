73e0d01a61828d160c18cd8c773c10ef34504504
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-02 15:20:30 +0000

    Update FacilColoc/resources/views/colocations/index.blade.php

--- FacilColoc/resources/views/colocations/index.blade.php
+++ FacilColoc/resources/views/colocations/index.blade.php
@@ -15,12 +15,17 @@
         <ul class="list-group">
 
             @foreach($colocations as $colocation)
-                <li class="list-group-item">
-
+                <li class="list-group-item d-flex justify-content-between align-items-center">
                     <a href="{{ route('colocations.show',$colocation->id) }}">
                         {{ $colocation->name }}
                     </a>
-
+                    @if($colocation->status === 'active')
+                        <span class="badge bg-success">Active</span>
+                    @elseif($colocation->status === 'inactive')
+                        <span class="badge bg-secondary">Inactive</span>
+                    @else
+                        <span class="badge bg-danger">Annulée</span>
+                    @endif
                 </li>
             @endforeach
 
