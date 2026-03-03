08b932c065da9d2cf556b7dfb898021894e38b59
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-03 18:56:03 +0000

    Update FacilColoc/resources/views/emails/invitation.blade.php

--- FacilColoc/resources/views/emails/invitation.blade.php
+++ FacilColoc/resources/views/emails/invitation.blade.php
@@ -7,7 +7,7 @@
 <body>
     <p>Bonjour,</p>
 
-    <p>Vous avez Ã©tÃ© invitÃ© Ã  rejoindre la colocation <strong>{{ $invitation->colocation->name }}</strong>.</p>
+    <p>Vous avez été invité à rejoindre la colocation <strong>{{ $invitation->colocation->name }}</strong>.</p>
 
     <p>Cliquez sur ce lien pour accepter ou refuser :</p>
 
@@ -17,9 +17,10 @@
         </a>
     </p>
 
-    <p>Si vous nâ€™Ãªtes pas concernÃ©, ignorez cet email.</p>
+    <p>Si vous n€™êtes pas concerné, ignorez cet email.</p>
 
-    <p>â€” FacileColoc</p>
+    <p>€” FacileColoc</p>
 </body>
 </html>
+
 
